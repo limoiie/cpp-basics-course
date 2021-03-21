b1279cf3296b68f8e73ebf672946a62c363ab4ea
limoiie <limo.iie4@gmail.com>
2021-03-21 20:00:31 +0800

    refactor: Adjust demo code for Overload Conversion Operator

--- Functions/Operator_Functions/Overload_Type_Conversion_Operators/src/demo.cpp
+++ Functions/Operator_Functions/Overload_Type_Conversion_Operators/src/demo.cpp
@@ -9,27 +9,27 @@ struct complex_int {
     /**
      * Support both implicit and explicit conversion to the target type `string`
      */
-    operator string() const; // NOLINT(google-explicit-constructor)
+    operator string() const { // NOLINT(google-explicit-constructor)
+        return string("complex_int(")
+               + std::to_string(i) + ", "
+               + std::to_string(j) + ")";
+    }
 
     /**
      * Support explict conversion to the target type `double`
      */
-    explicit operator double() const;
+    explicit operator double() const {
+        return sqrt(pow(i, 2) + pow(j, 2));
+    }
 };
 
-complex_int::operator string() const {
-    return string("complex_int(")
-           + std::to_string(i) + ", "
-           + std::to_string(j) + ")";
-}
-
-DEMO(OverloadTypeConversioinOperators, Explicit) { // NOLINT(cert-err58-cpp)
+DEMO(OverloadTypeConversioinOperators, Implicit) { // NOLINT(cert-err58-cpp)
 
     complex_int v = {3, 4};
 
-    string s1 = v;                       // 支持 implicitly conversion
-    string s3 = (string) v;              // 老式 explicitly conversion (C++11 之前)
-    string s2 = static_cast<string>(v);  // 新式 explicitly conversion (C++11 之后)
+    string s1 = v;                       // support implicitly conversion
+    string s3 = (string) v;              // support C-style explicitly conversion
+    string s2 = static_cast<string>(v);  // support C++-style explicitly conversion
 
     std::cout << "implicit: " << s1 << std::endl;
     std::cout << "(std::string) v: " << s2 << std::endl;
@@ -37,19 +37,15 @@ DEMO(OverloadTypeConversioinOperators, Explicit) { // NOLINT(cert-err58-cpp)
 
 }
 
-complex_int::operator double() const {
-    return sqrt(pow(i, 2) + pow(j, 2));
-}
-
-DEMO(OverloadTypeConversioinOperators, Implicit) { // NOLINT(cert-err58-cpp)
+DEMO(OverloadTypeConversioinOperators, Explicit) { // NOLINT(cert-err58-cpp)
 
     complex_int v = {3, 4};
 
-    // 不支持 implicitly conversion
+    // do not support implicitly conversion
     // double a = v; // error: no viable implicit conversion
 
-    auto a = (double) v;             // 老式 explicitly conversion (C++11 之前)
-    auto b = static_cast<double>(v); // 新式 explicitly conversion (C++11 之后)
+    auto a = (double) v;             // support C-style explicitly conversion
+    auto b = static_cast<double>(v); // support C++-style explicitly conversion
 
     std::cout << "(double) v: " << a << std::endl;
     std::cout << "static_cast<double>(v): " << b << std::endl;
